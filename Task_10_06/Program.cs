7047a6c3ff02505c1fef1cc42f60c0200674d632
xtao692@gmail.com <xtao692@gmail.com>
2025-03-19 17:34:24 +0500

    выполнена 6 задача 10 урока

---
+++ Task_10_06/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Task_10_06
+{
+    internal class Program
+    { 
+        /* Создать Метод ArrayGeneration не возвращающий значения,
+       принимает целое число n,
+       выводит наконсольсгенерированный массив размерности n*n. */
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размерность массива n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            ArrayGeneration(n);
+        }
+
+        /// <summary>
+        /// Генерирует и выводит массив размерности n*n.
+        /// </summary>
+        /// <param name="n">Размерность массива.</param>
+ 
+        static void ArrayGeneration(int n)
+        {
+            int[,] array = new int[n, n]; // Создаем двумерный массив размером n*n
+
+            Random random = new Random(); // Заполняем массив случайными числами
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = random.Next(1, 101); // Случайные числа от 1 до 100
+                }
+            }
+
+            // Выводим массив в консоль
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j] + "\t"); // Выводим элемент массива с табуляцией
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
