8f02aa65d0433b0a252e33ef0577c819dc4a964c
xtao692@gmail.com <xtao692@gmail.com>
2025-05-02 01:40:23 +0500

    выполнена 1 задача 33 урока

---
+++ Task_33_01/Rectangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_33_01
+{
+    internal class Rectangle : IDrawable
+    {
+        private int firstcatheter;
+        private int secondcatheter;
+        public int Firstcatheter
+        {
+            get => firstcatheter;
+            set
+            {
+                if (value > 1)
+                    firstcatheter = value;
+                else
+                    throw new ArgumentException("Катет не должен быть меньше или равен 1");
+            }
+        }
+
+        public int Secondcatheter
+        {
+            get => secondcatheter;
+            set
+            {
+                if (value > 1)
+                    secondcatheter = value;
+                else
+                    throw new ArgumentException("Катет не должен быть меньше или равен 1");
+            }
+        }
+
+        public Rectangle(int height, int width)
+        {
+            firstcatheter = height;
+            secondcatheter = width;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine();
+
+
+            for (int i = 1; i <= firstcatheter; i++)
+            {
+                int stars = (int)Math.Round((double)i / firstcatheter * secondcatheter);
+
+                for (int j = 0; j < stars; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
